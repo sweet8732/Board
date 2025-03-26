fa2c48eae366859f254cb3c8aeca8d953d7edf69
sweet8732 <sweet8732@nate.com>
2025-03-26 21:23:46 +0900

    2025-03-26

--- src/main/java/com/example/board/BoardApplication.java
+++ src/main/java/com/example/board/BoardApplication.java
@@ -2,12 +2,19 @@ package com.example.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class BoardApplication {
+public class BoardApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BoardApplication.class, args);
+    }
 
+    // WAR 배포를 위한 설정
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BoardApplication.class);
+    }
 }
